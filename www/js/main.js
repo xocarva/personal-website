787354d306b0f71701cbca0c2cb4db520f01155d
xocarva <correo@xoancarneiro.com>
2022-05-31 12:52:43 +0200

    modify project card hover

--- www/js/main.js
+++ www/js/main.js
@@ -35,31 +35,61 @@ const loadProjects = async () => {
         header.className = 'card-header';
         card.appendChild( header );
 
-        const h3 = document.createElement( 'h3' );
-        h3.className = 'card-title';
-        header.appendChild( h3 );
-
-        const demoLink = document.createElement( 'a' );
-        demoLink.href = projects[ key ].url;
-        demoLink.target = '_blank';
-        demoLink.rel = 'noreferrer noopener';
-        demoLink.title = `${ projects[ key ].title } demo`;
-        demoLink.innerText = projects[ key ].title;
-        h3.appendChild( demoLink );
+        const title = document.createElement( 'h3' );
+        title.className = 'card-title';
+        title.innerText = projects[ key ].title;
+        header.appendChild( title );
 
         const description = document.createElement( 'p' );
         description.className = 'card-description';
         description.innerText = projects[ key ].description;
         header.appendChild( description );
 
-        const screenshot = document.createElement( 'a' );
-        screenshot.className = 'card-screenshot';
-        screenshot.style = `background-image: url(${ projects[ key ].image })`;
-        screenshot.href = projects[ key ].url;
-        screenshot.target = '_blank';
-        screenshot.rel = 'noreferrer noopener';
-        screenshot.title = `${ projects[ key ].title } demo`;
-        card.appendChild( screenshot );
+        const main = document.createElement( 'main' );
+        main.className = 'card-main';
+        card.appendChild( main );
+
+        const demo = document.createElement( 'div' );
+        demo.className = 'demo';
+        main.appendChild( demo );
+
+        const demoLink = document.createElement( 'a' );
+        demoLink.className = 'demo-link';
+        demoLink.href = projects[ key ].url;
+        demoLink.target = '_blank';
+        demoLink.rel = 'noreferrer noopener';
+        demoLink.title = `${ projects[ key ].title } demo`;
+        demo.appendChild( demoLink );
+
+        const demoIcon = document.createElement( 'i' );
+        demoIcon.className = 'fa-solid fa-eye';
+        demoLink.appendChild( demoIcon );
+
+        const demoText = document.createElement( 'span' );
+        demoText.className = 'demo-text';
+        demoText.innerText = 'demo';
+        demo.appendChild( demoText );
+
+        const repo = document.createElement( 'div' );
+        repo.className = 'repo';
+        main.appendChild( repo );
+
+        const repoLink = document.createElement( 'a' );
+        repoLink.className = 'repo-link';
+        repoLink.href = projects[ key ].repository;
+        repoLink.target = '_blank';
+        repoLink.rel = 'noreferrer noopener';
+        repoLink.title = `${ projects[ key ].title } repo`;
+        repo.appendChild( repoLink );
+
+        const repoIcon = document.createElement( 'i' );
+        repoIcon.className = 'fa-brands fa-github';
+        repoLink.appendChild( repoIcon );
+
+        const repoText = document.createElement( 'span' );
+        repoText.className = 'repo-text';
+        repoText.innerText = 'repo';
+        repo.appendChild( repoText );
 
         const footer = document.createElement( 'footer' );
         footer.className = 'card-footer';
@@ -74,34 +104,6 @@ const loadProjects = async () => {
             cardStack.appendChild( stackIcon );
         });
         footer.appendChild( cardStack );
-
-        const cardLinks = document.createElement( 'span' );
-        cardLinks.className = 'card-links';
-        footer.appendChild( cardLinks );
-
-        const demo = document.createElement( 'a' );
-        demo.className = 'demo';
-        demo.href = projects[ key ].url;
-        demo.target = '_blank';
-        demo.rel = 'noreferrer noopener';
-        demo.title = `${ projects[ key ].title } demo`
-        cardLinks.appendChild( demo );
-
-        const demoIcon = document.createElement( 'i' );
-        demoIcon.className = 'fa-solid fa-eye';
-        demo.appendChild( demoIcon );
-
-        const repo = document.createElement( 'a' );
-        repo.className = 'repo';
-        repo.href = projects[ key ].repository;
-        repo.target = '_blank';
-        repo.rel = 'noreferrer noopener';
-        repo.title = `${ projects[ key ].title } repo`
-        cardLinks.appendChild( repo );
-
-        const repoIcon = document.createElement( 'i' );
-        repoIcon.className = 'fa-brands fa-github';
-        repo.appendChild( repoIcon );
 
         projectsGrid.appendChild( card );
 
